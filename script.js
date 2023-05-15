5342a101d3039b2d8bb52e9ac1f8278b14bf8a37
Vivek <vivek.mitra@outlook.com>
2023-05-15 20:11:28 +1000

    js started and final photo pof calculator added

---
+++ script.js
@@ -0,0 +1,8 @@
+//javascript file which contains the calculator logic i.e. the brains
+
+//functions
+
+const addNum = (a, b) => { return a + b; }
+const subtNum = (a, b) => { return a - b; }
+const multNum = (a, b) => { return a * b; }
+const divNum = (a, b) => { return a / b; }
