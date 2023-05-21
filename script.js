c36f56cf773b5c50624a8aa2d95002580044f6fd
Vivek <vivek.mitra@outlook.com>
2023-05-21 12:05:09 +0530

    calculator done

--- script.js
+++ script.js
@@ -12,39 +12,84 @@ const divNum = (a, b) => {
     } 
     else return a / b ;}
 
-let num1;
-let num2;
-let opVal;
+function calcBrain(firstVal, secondVal, opVal) {
+    if (opVal === "+") {
+       return addNum(firstVal, secondVal);
+        }
+    else if(opVal === "-") {
+        return subtNum(firstVal, secondVal);
+    }
+    else if(opVal === "*") {
+        return multNum(firstVal, secondVal);
+    }
+    else if(opVal === "/") {
+        return divNum(firstVal, secondVal);
+    }
+    else {
+        return "Error!";
+    }
+}
 
-function operate(firstVal, secondVal, opCode) {
-    //if 'opcode == +',
-    //return addNum(firstVal, secondVal)
+function operate() {
+    let firstVal, secondVal, opVal;
+    
+    let calculationsDone = false;
 
-    //if 'opcode === '-',
-    //return subtNum(firstVal, secondVal);
+    let opCode = document.querySelectorAll('#math-key');
+    let calcDisplay = document.querySelector('#calculator-display');
+    let opKey = document.querySelector("#op-key");
+   
+    opCode.forEach((key) => { 
+        key.addEventListener('mousedown', () => {
+        if(firstVal && !calculationsDone) {
+            secondVal = parseFloat(calcDisplay.textContent)
+            calcDisplay.textContent = calcBrain(firstVal, secondVal, opVal);
+            firstVal = parseFloat(calcDisplay.textContent);
+        } else {
+            firstVal = parseFloat(calcDisplay.textContent);
+            calcDisplay.textContent = '';
+        }
+        opVal = key.getAttribute('value');
+        calculationsDone = false;
+        });
+    });
 
-    //if 'opcode === '*',
-    //return multNum(firstVal, secondVal);
-
-    //if 'opcode === '/',
-    //return divNum(firstVal, secondVal);
+    //when "=" is hit, perform the operation 
+    
+    opKey.addEventListener('mousedown', () => {
+        secondVal = parseFloat(calcDisplay.textContent);
+        calcDisplay.innerHTML = calcBrain(firstVal, secondVal, opVal);
+        firstVal = parseFloat(calcDisplay.textContent);
+        calculationsDone = true;
+    });
 }
 
 function clear() {
     //if buttonValue === "clear"
         //clear thr display
-        //set memory variable to 0
+        //set display to ""
+        let calcDisplay = document.querySelector('#calculator-display');
+        let clearKey = document.querySelector('#clear-btn');
+        clearKey.addEventListener('mousedown', () => {        //clear the calculator display when clear button is pressed
+            calcDisplay.innerHTML = '';
+        });
 }
 
-function display() {
-    //document.appendChild(buttonValues)
-    let displayVal;
-   
-    let keys = document.querySelectorAll('#key');
-    let clearKey = document.querySelector('#clear-btn');
-    let deleteKey = document.querySelector('#delete-btn');
-    let calcDisplay = document.querySelector('#calculator-display');
+function deleteDisplay() {
 
+    let calcDisplay = document.querySelector('#calculator-display');
+    //function to delete the most recently entered in display
+    let deleteKey = document.querySelector('#delete-btn');
+    deleteKey.addEventListener('mousedown', () => {       //start deleting the most recent entered character in the display
+        calcDisplay.textContent = calcDisplay.textContent.slice(0, -1);
+     });
+}
+
+function display() {                         //function to add number to the calculator display 
+                                             //a key is pressed on the keyboard
+    let displayVal;
+    let keys = document.querySelectorAll('#key');
+    let calcDisplay = document.querySelector('#calculator-display');
 
     keys.forEach((key) => {
         key.addEventListener('mousedown', () => {         //every time a button is pressed, get value
@@ -53,26 +98,10 @@ function display() {
             calcDisplay.textContent += displayVal;        //insert the variable value into the display field
         });
     });
-    
-    clearKey.addEventListener('mousedown', () => {        //clear the calculator display when clear button is pressed
-        calcDisplay.innerHTML = '';
-    });
-    
-    deleteKey.addEventListener('mousedown', () => {       //start deleting the most recent entered character in the display
-       calcDisplay.textContent = calcDisplay.textContent.slice(0, -1);
-    })
-}
-
-function calcBrain() {
-    //when display() contains first num and second num and user presses = 
-    //call operate()
-
-    //when display contains first num and second num and user presses another
-    //opCode, call operate() and store it in another variable(memoryVal) and 
-    //if user presses another opcode, call operate() with the current value 
-    //and button value pressed by user and update the memoryVal
-
 
 }
 
 display();
+operate();
+clear();
+deleteDisplay();
